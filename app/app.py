db29691efa541a8ad2c349545907b8e54b17c599
Rene Kumsta <rck@sequensis.de>
2022-11-07 19:55:04 +0100

    Corrected read file

--- app/app.py
+++ app/app.py
@@ -1,5 +1,34 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
+import seaborn as sns
+
+
 
 st.title('Planets Dashboard')
+
+
+# Load function
+#@st.cache
+def load_data():
+    df = pd.read_csv('../data/data.csv')
+    # df = df1.T
+    # header = df.iloc[0]
+    # df = df[1:]
+    # df.columns = header
+    return df
+
+
+# Load data
+with st.spinner('Loading data...'):
+    df = load_data()
+st.success('Loading data... Done!')
+
+
+# Create a subheader
+st.subheader('Raw data')
+st.write(df)
+
+st.subheader('Distance from Earth')
+dist_values = sns.barplot(data=df, x='')
